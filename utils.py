033ee89147e7c909c309c0a0ebeeeaba698894af
Gustavo Almeida <110310596+goalmeida05@users.noreply.github.com>
2024-04-04 16:36:03 -0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,233 @@
+aminos = {
+   "A":{
+      "id": 0,
+      "3letter":"Ala",
+      "nH":7,
+      "nC":3,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      'group': 'alphaticr'
+   },
+   "R":{
+      "id": 14,
+      "3letter":"Arg",
+      "nH":14,
+      "nC":6,
+      "nN":4,
+      "nO":2,
+      "nS":0,
+      "group": "postivecharger"
+   },
+   "N":{
+      "id": 11,
+      "3letter":"Asn",
+      "nH":8,
+      "nC":4,
+      "nN":2,
+      "nO":3,
+      "nS":0,
+      "group": "uncharger"
+   },
+   "D":{
+      "id": 2,
+      "3letter":"Asp",
+      "nH":7,
+      "nC":4,
+      "nN":1,
+      "nO":4,
+      "nS":0,
+      "group": "negativecharger"
+   },
+   "C":{
+      "id": 1,
+      "3letter":"Cys",
+      "nH":7,
+      "nC":3,
+      "nN":1,
+      "nO":2,
+      "nS":1,
+      "group": "uncharger"
+   },
+   "Q":{
+      "id": 13,
+      "3letter":"Gln",
+      "nH":10,
+      "nC":5,
+      "nN":2,
+      "nO":3,
+      "nS":0,
+      "group": "uncharger"
+   },
+   "E":{
+      "id": 3,
+      "3letter":"Glu",
+      "nH":9,
+      "nC":5,
+      "nN":1,
+      "nO":4,
+      "nS":0,
+      "group": "negativecharger"
+   },
+   "G":{
+      "id": 5,
+      "3letter":"Gly",
+      "nH":5,
+      "nC":2,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "alphaticr"
+   },
+   "H":{
+      "id": 6,
+      "3letter":"His",
+      "nH":9,
+      "nC":6,
+      "nN":3,
+      "nO":2,
+      "nS":0,
+      "group": "postivecharger"
+   },
+   "I":{
+      "id": 7,
+      "3letter":"Ile",
+      "nH":13,
+      "nC":6,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "alphaticr"
+   },
+   "L":{
+      "id": 9,
+      "3letter":"Leu",
+      "nH":13,
+      "nC":6,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "alphaticr"
+   },
+   "K":{
+      "id": 8,
+      "3letter":"Lys",
+      "nH":14,
+      "nC":6,
+      "nN":2,
+      "nO":2,
+      "nS":0,
+      "group": "postivecharger"
+   },
+   "M":{
+      "id": 10,
+      "3letter":"Met",
+      "nH":11,
+      "nC":5,
+      "nN":1,
+      "nO":2,
+      "nS":1,
+      "group": "alphaticr"
+   },
+   "F":{
+      "id": 4,
+      "3letter":"Phe",
+      "nH":11,
+      "nC":9,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "aromatic"
+   },
+   "P":{
+      "id": 12,
+      "3letter":"Pro",
+      "nH":9,
+      "nC":5,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "uncharger"
+   },
+   "S":{
+      "id": 15,
+      "3letter":"Ser",
+      "nH":7,
+      "nC":3,
+      "nN":1,
+      "nO":3,
+      "nS":0,
+      "group": "uncharger"
+   },
+   "T":{
+      "id": 16,
+      "3letter":"Thr",
+      "nH":9,
+      "nC":4,
+      "nN":1,
+      "nO":3,
+      "nS":0,
+      "group": "uncharger"
+   },
+   "W":{
+      "id": 18,
+      "3letter":"Trp",
+      "nH":12,
+      "nC":11,
+      "nN":2,
+      "nO":2,
+      "nS":0,
+      "group": "aromatic"
+   },
+   "Y":{
+      "id": 19,
+      "3letter":"Tyr",
+      "nH":11,
+      "nC":9,
+      "nN":1,
+      "nO":3,
+      "nS":0,
+      "group": "aromatic"
+   },
+   "V":{
+      "id": 17,
+      "3letter":"Val",
+      "nH":11,
+      "nC":5,
+      "nN":1,
+      "nO":2,
+      "nS":0,
+      "group": "alphaticr"
+   }
+}
+
+group = {
+		'alphaticr': 0,
+		'aromatic': 1,
+		'postivecharger': 2,
+		'negativecharger': 3,
+		'uncharger': 4
+	}
+	
+aminos_id = {
+    "A": 0,
+    "C": 1,
+    "D": 2,
+    "E": 3,
+    "F": 4,
+    "G": 5,
+    "H": 6,
+    "I": 7,
+    "K": 8,
+    "L": 9,
+    "M": 10,
+    "N": 11,
+    "P": 12,
+    "Q": 13,
+    "R": 14,
+    "S": 15,
+    "T": 16,
+    "V": 17,
+    "W": 18,
+    "Y": 19
+}
